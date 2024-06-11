e518441ae1e85d7d8290b06cac1099f98fc2f1d5
SanaMehmood16 <sabbaxi16@gmail.com>
2024-06-11 14:47:01 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+import Navbar from "../src/componenets/navbar"
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componenets/navbar.js
@@ -0,0 +1,252 @@
+import React from "react";
+import "./navbar.css"
+import logo from "../images/drumelia logo.svg"
+import footerimg from "../images/footer.jpg"
+
+
+function Navbar() {
+  return (
+<section>
+<div className="backgroundimg">
+      
+      <div className="navbar">
+        <ul>
+            <li><a href="home">HOME</a></li>
+            <li><a href="PROPERTIES">PROPERTIES</a></li>
+            <li><a href="TOURS">TOURS</a></li>
+            <div className="con2">
+            <img src={logo} alt="" />
+            </div>
+            <li><a href="GUIDE">GUIDE</a></li>
+            <li><a href="ABOUT">ABOUT</a></li>
+            <li><a href="CONTACT">CONTACT</a></li>
+
+        </ul>
+      </div>
+      <div className="maintext">
+        <h2>THE #1 SOURCE FOR</h2>
+        <h1>MARBELLA</h1>
+        <h2>LUXURY REAL ESTATE,<span> FIND YOURS.</span></h2>
+        <h6>Best Real Estate Agency in Spain 2022-2024 according to International Property Awards</h6>
+      </div>
+    </div>
+    <section>
+    <div className="DrumeliaAbout">
+     <h1>DRUMELIA</h1>
+     <p><b>REAL ESTATE</b></p>
+     <p>Discover the epitome of luxury living with Drumelia, a world-renowned high-end real estate agency based in
+         Marbella. With decades of experience, we are dedicated to revolutionising the industry and delivering 
+         unparalleled service to buyers and vendors searching for exclusive properties in the Marbella area.</p>
+         <div className="DrumeliaAboutText">
+            <div className="DrumeliaAboutText1"> 
+                <p>For those searching for the finest properties in the Marbella area, our hand-picked collection 
+                    of homes is carefully curated to meet your specific needs and desires. Our team of 45 experts 
+                    is dedicated to finding the perfect high-end home that exceeds your expectations and providing
+                     you with the highest level of service and support. At Drumelia, real estate is not just a 
+                     passion; it’s a way of life</p>
+                     <p>Our vendors benefit from our exceptional online presence and expertly crafted property marketing strategies, which reach a global audience. Trust us to showcase your property to its fullest potential and achieve outstanding results through our 
+                        effective property marketing strategies in the Marbella area.</p>
+                        <p>Our team of multilingual experts (15 languages and counting) is dedicated to providing the best possible service to our clients. As a leader in the Marbella high-end real estate market, we are committed to 
+                            setting and exceeding the standards of excellence.</p>
+                            <p>Join the Drumelia family and experience the difference for yourself in Marbella’s 
+                                premium real estate market.</p>
+            </div>
+            <div className="DrumeliaAboutTextButton">
+                <button>
+                    <h1>SELL</h1>
+                    <p>YOUR HOME</p>
+                </button>
+                <button>
+                <h1>BUY</h1>
+                    <p>YOUR HOME</p>
+                </button>
+                <button>
+                <h1>ABOUT</h1>
+                    <p>DRUMELIA REAL ESTATE</p>
+                </button>
+            </div>
+
+         </div>
+   </div>
+    </section>
+    <section>
+        <div className="teamImg">
+            <span>
+              <h1>THE TEAM</h1>
+              <h6>MEAT US</h6>
+            </span>
+        </div>
+    </section>
+    <section>
+      <div className="listingtext">
+        <h1>EXCLUSIVE LISTINGS TO DRUMELIA</h1>
+        <p>#1 sales performance for exclusive homes in Marbella.</p>
+      </div>
+      <div className="listingImg">
+        <div className="listimg1">
+          <span>
+          <h1>VILLA CALMA</h1>
+          <h6>MARBELLA CLUB GOLF RESORT</h6>
+          <p>€6,750,000</p>
+          </span>
+
+        </div>
+        <div className="listimg2">
+        <span>
+        <h1>Villa Graciosa</h1>
+          <h6>MARBELLA CLUB GOLF RESORT</h6>
+          <p>€6,750,000</p>
+        </span>
+        </div>
+        <div className="listimg3">
+        <span>
+        <h1>VILLA TOCCATA</h1>
+          <h6>MARBELLA CLUB GOLF RESORT</h6>
+          <p>€6,750,000</p>
+        </span>
+        </div>
+        <div className="listimg4">
+        <span>
+        <h1>CASA FLOTANTE</h1>
+          <h6>MARBELLA CLUB GOLF RESORT</h6>
+          <p>€6,750,000</p>
+        </span>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className="priceimg">
+        <span>
+          <h1> Villa Atlas, La Reserva </h1>
+          <p><b>Villa Atlas,LA PRESERVA</b></p>
+          <p>Villa Atlas - Stunning contemporary-style luxury villa with sea panorama in prestigious La Reserva de Sotogrande.. Embrace a modern, sophisticated lifestyle in this exceptional high-end house in Sotogrande, while enjoying close proximity to the elite resort town, sea, port and golf!</p>
+          <p><b>Property Video & Details ›</b></p>
+
+        </span>
+      </div>
+    </section>
+    <section>
+    <div className="DrumeliaAbout">
+     <h1>PROVEN MARKETING</h1>
+     <p><b>FOR PROPERTY OWNERS</b></p>
+     <p>Discover the epitome of luxury living with Drumelia, a world-renowned high-end real estate agency based in
+         Marbella. With decades of experience, we are dedicated to revolutionising the industry and delivering 
+         unparalleled service to buyers and vendors searching for exclusive properties in the Marbella area.</p>
+         <div className="DrumeliaAboutText">
+            <div className="DrumeliaAboutText1"> 
+                <p>For those searching for the finest properties in the Marbella area, our hand-picked collection 
+                    of homes is carefully curated to meet your specific needs and desires. Our team of 45 experts 
+                    is dedicated to finding the perfect high-end home that exceeds your expectations and providing
+                     you with the highest level of service and support. At Drumelia, real estate is not just a 
+                     passion; it’s a way of life</p>
+                     <p>Our vendors benefit from our exceptional online presence and expertly crafted property marketing strategies, which reach a global audience. Trust us to showcase your property to its fullest potential and achieve outstanding results through our 
+                        effective property marketing strategies in the Marbella area.</p>
+                        <p>Our team of multilingual experts (15 languages and counting) is dedicated to providing the best possible service to our clients. As a leader in the Marbella high-end real estate market, we are committed to 
+                            setting and exceeding the standards of excellence.</p>
+                            <p>Join the Drumelia family and experience the difference for yourself in Marbella’s 
+                                premium real estate market.</p>
+            </div>
+            <div className="marketingButton">
+                <button>
+                    <h1>YOUTUBE</h1>
+                    <p>+ 190K SUBSCRIBERS</p>
+                </button>
+                <button>
+                <h1>INSTAGRAM</h1>
+                    <p>+ 160K FOLLOWERS</p>
+                </button>
+                <button>
+                <h1>MARKETING</h1>
+                    <p>YOUR HOME</p>
+                </button>
+            </div>
+
+         </div>
+   </div>
+    </section>
+    <section>
+      <div className="drumaliaImgbaground">
+        
+      </div>
+    </section>
+    <section>
+      <footer className="footerNAv">
+      <span>
+      <a href="TOP AREAS"><b>TOP AREAS</b></a>
+           <a href="Sierra Blanca">Sierra Blanca</a>
+          <a href="La Zagaleta">La Zagaleta</a>
+           <a href="Golden Mile">Golden Mile</a>
+        
+      </span>
+      </footer>
+      <div className="footerdiv">
+        <div className="footertxt">
+          <span>
+            <h1>Know everything that hits the market</h1>
+            <p>Get private announcements and priority content.</p>
+            <div className="searchbar">
+            <input type="text" placeholder="Enter Your Mail"  />
+            <button>Subcribe</button>
+            </div>
+            <div className="checkbox">
+              <input type="checkbox" /><p>I agree to receive info by email and I accept the privacy policy</p>
+            </div>
+          </span>
+          <div className="footerpages">
+            <div className="explore">
+              <h1>EXPLORE</h1>
+              <ul>
+                <li>Home</li>
+                <li>Properties</li>
+                <li>Tours</li>
+                <li>Guides</li>
+                <li>Blog</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Careers</li>
+              </ul>
+            </div>
+            <div className="visit">
+              <h1>VISIT</h1>
+            <ul>
+                <li>Drumelia Real Estate Marbella</li>
+                <li>Centro de Negocios Puerta de Banus</li>
+                <li>Edificio B, Local 11</li>
+                <li>29660 Marbella</li>
+                <li>View on Google Maps</li>
+              </ul>
+              <div className="business">
+                <h1>BUSINESS</h1>
+                <ul>
+                  <li>+34 952 766 950</li>
+                  <li>info@drumelia.com</li>
+                </ul>
+              </div>
+            </div>
+            <div className="follow">
+              <h1>FOLLOW</h1>
+              <ul>
+                <li>Youtube</li>
+                <li>Instagram</li>
+                <li>Linkedin</li>
+                <li>Twitter</li>
+                <li>Facebook</li>
+                <li>Pinterest</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="footerImg">
+          <img src={footerimg} alt="" />
+        </div>
+      </div>
+      <div className="footerpara">
+        <p><b>© 2023 Drumelia Real Estate · <span>Website use terms and conditions · Cookies Policy</span> · designed and built by <span>inmoba.com</span></b></p>
+      </div>
+      
+    </section>
+</section>
+  )
+}
+
+export default Navbar;
